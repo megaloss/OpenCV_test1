e727695859b2001e521918843beb5a98a7ab544a
Yury Shchelkanov <megalosslj@gmail.com>
2020-12-14 10:58:58 +0100

    Add files via upload
    Changed the code completely, added more flexibility and threading/multiprocessing. Now you can choose ROI (region of interest) at start and adjust parameters for masking on-the-go.

--- main.py
+++ main.py
@@ -1,77 +1,195 @@
 import cv2
-import pafy
 import numpy as np
 import pytesseract
-import time
+import pafy
+import concurrent.futures
 
-url = "https://www.youtube.com/watch?v=Li3FPoa3iYE"
-#Here we define a mask found by detect_colours.py
-lower=np.array([0,100,130]) #min HSV
-upper=np.array([60,180,250]) #max HSV
-#define an area range of license plate's contour
-lp_max=550
-lp_min=480
+url= 'https://www.youtube.com/watch?v=Li3FPoa3iYE'
+num_set = set()
+jobs=[]
+
+
+h_min=8
+h_max=21
+
+s_min=112
+s_max=205
+
+v_min=75
+v_max=215
+pause=False
+show_mask = True
+
+def set_mask(_):
+    global lower, upper
+    lower = np.array([cv2.getTrackbarPos("Hue_min", "params"), cv2.getTrackbarPos("Sat_min", "params"),
+                      cv2.getTrackbarPos("Val_min", "params")])
+    upper = np.array([cv2.getTrackbarPos("Hue_max", "params"), cv2.getTrackbarPos("Sat_max", "params"),
+                      cv2.getTrackbarPos("Val_max", "params")])
+    return ()
+
+
+lp_max=1000
+lp_min=400
 counter=0
-#tes_config = r'''tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --oem 3 --psm 13'''
-#tes_congis = r'--oem 3 --psm 6 -c tessedit_char_whitelist = ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+tilt=1
+starting_no=590
 
-#opening youtube stream
 video = pafy.new(url)
 best = video.getbest(preftype="mp4")
 
 capture = cv2.VideoCapture()
 capture.open(best.url)
-#cap=cv2.VideoCapture(0)
-#cap.set(3,640)
-#cap.set(4,480)
-capture.set(10,10)
+s, img = capture.read()
+# x_img_min = 300
+# x_img_max = -1000
+# y_img_min = 700
+# y_img_max = -100
 
-success, img = capture.read()
-print("Image size: ",img.shape)
+x_img_min = 400
+x_img_max = -950
+y_img_min = 800
+y_img_max = -1
+
+
+roi = cv2.selectROI(img)
+cv2.destroyAllWindows()
+
+cv2.namedWindow("params")
+cv2.resizeWindow("params",400,200)
+cv2.createTrackbar("Hue_min","params",h_min,178,set_mask)
+cv2.createTrackbar("Hue_max","params",h_max,178,set_mask)
+cv2.createTrackbar("Sat_min","params",s_min,255,set_mask)
+cv2.createTrackbar("Sat_max","params",s_max,255,set_mask)
+cv2.createTrackbar("Val_min","params",v_min,255,set_mask)
+cv2.createTrackbar("Val_max","params",v_max,255,set_mask)
+
+lower=np.array([cv2.getTrackbarPos("Hue_min","params"),cv2.getTrackbarPos("Sat_min","params"),cv2.getTrackbarPos("Val_min","params")])
+upper=np.array([cv2.getTrackbarPos("Hue_max","params"),cv2.getTrackbarPos("Sat_max","params"),cv2.getTrackbarPos("Val_max","params")])
+
+
+img = img[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+min_diff = roi[2]*roi[3]/5 #we ignore if frame is changed in less pixels than min_diff
+#img = img[800:, 200:-800]
+#img = img[y_img_min:y_img_max, x_img_min:x_img_max]
+img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+old_img_gray=np.copy(img_gray)
+
+def write_lp(text,number):
+    global num_set
+    print('checking')
+    if text in num_set: return False
+    cv2.imwrite(text+".png",number)
+    print ('saving')
+    return True
+
+
+
+def recognize(img):
+    text = pytesseract.image_to_string(img,
+                                      config='--user-patterns pattern.txt --oem 3 --psm 13 -c tessedit_char_whitelist=-BDFGHJKLNPRSTUVXYZ0123456789  load_system_dawg=false load_freq_dawg=false')
+    return text
+
+
+processor = concurrent.futures.ProcessPoolExecutor()
+threader = concurrent.futures.ThreadPoolExecutor()
+
 while True:
-    _, img = capture.read()
-    img=img[900:1080,500:800] #this is where the cars pass by in a certain direction
-    #cv2.imshow("Video",img[900:1080,500:800])
+    if pause==False:
+        old_img_gray=np.copy(img_gray)
+        s, img = capture.read()
+        img = img[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+        img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        k = np.sum(img_gray != old_img_gray)
+        if k < min_diff:
+            #cv2.putText(img_gray,"skipping", (20,20),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255))
+            #print('skipping, k=',k)
+            #cv2.imshow('Contours',img_gray)
+            continue
+        #cv2.putText(img, 'running', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
+
+
+
+
+    if s!=True:
+        continue
+
+
     img_hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     img_blur=cv2.blur(img_hsv,(10,10),2)
-    
-    #applying mask and detecting contours 
-    mask = cv2.inRange(img_blur, lower, upper)
-    contours,_=cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    
+    img_eroded=cv2.erode(img_blur,(30,30),iterations=2)
+    mask = cv2.inRange(img_eroded, lower, upper)
+
+    if show_mask: cv2.imshow('mask', mask)
+    contours,_=cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    img_copy = np.copy(img)
     for cont in contours:
         area=cv2.contourArea(cont)
-        #print("area:",area)
-        if lp_max > area and area > lp_min: #Choosing only contours with a certain area
+
+
+        if lp_max > area and area > lp_min:
             box=cv2.approxPolyDP(cont,1,1)
             x,y,w,h=cv2.boundingRect(box)
-            #cv2.drawContours(img, cont, -1,(255,0,0),2)
-            #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),1)
+
+            cv2.rectangle(img_copy,(x,y),(x+w,y+h),(255,0,0),3)
 
 
-            number=img[y-2:y+h+2,x-2:x+w+2] # making a snapshot of a detected contour
-            print(number.shape)
-            #print(img_snap.shape[1])
-            if number.shape[0]>10 and number.shape[1]>40: #Checking if it has a proper shape of a typical license plate  
-                num_bw=cv2.cvtColor(number,cv2.COLOR_BGR2GRAY)
-                cv2.imshow('snap',num_bw)
-                cv2.imwrite("lp"+str(counter)+".png",num_bw)
+            number=img[y-2:y+h+2,x-3:x+w]
+
+            if (h * w < 100):
+                continue
+
+            if 3 < (w / h) < 6:
+
+                try:
+                    #cv2.imshow('snap',number)
+                    cv2.imshow('params', number)
+                except:
+                    print (number.shape)
+                    continue
+                if tilt != 0:
+                    pts1 = np.float32([[0, 0+tilt],[number.shape[0], 0], [0,number.shape[1]], [number.shape[0],number.shape[1]-tilt]])
+                    pts2 = np.float32([[0, 0],[number.shape[0],0], [0,number.shape[1]], [number.shape[0],number.shape[1]]])
+
+                    M = cv2.getPerspectiveTransform(pts1, pts2)
+                    number = cv2.warpPerspective(number, M, (number.shape[1], number.shape[0]) )
+                jobs.append(processor.submit (recognize,number))
+
+
+                for job in jobs:
+                    text=job.result()
+                    if text:
+                        text=text.strip()
+                        if len(text)>5: print ('Read: ',text, 'len:' , len(text))
+                        if len(text) != 6: continue
+                        threader.submit (write_lp,text,number)
+                        jobs.remove(job)
+
+
                 counter+=1
-
-                #trying ro recognize the number
-                text=pytesseract.image_to_string(num_bw, lang ='eng', config ='--oem 3 --psm 6 -c tessedit_char_whitelist=-ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-                if len(text)>5: #number usually consists from 6 char/digits
-                    print(text)
-                for i in range (10):
-                    _, img = capture.read()
+                #if pause==False:
+                #    for i in range(20):
+                #        _, _ = capture.read()
 
 
 
 
-    #cv2.imshow("Video", img)
-    #cv2.imshow("Mask", mask)
+    cv2.imshow("Contours", img_copy)
 
 
-    #saving snapshot of a license plate to file
-    if cv2.waitKey(5) & 0xFF == ord ('s'):
+    key= cv2.waitKey(1) & 0xFF
+    if key == ord ('s'):
         cv2.imwrite("car"+str(counter)+".png",img)
+    elif key == ord ('q'):
+        break
+    elif key == ord('m'):
+        if show_mask:
+            cv2.destroyWindow('mask')
+        show_mask = not show_mask
+    elif key == ord ('p'):
+        pause = not pause
+
+
+
+capture.release()
+cv2.destroyAllWindows()
